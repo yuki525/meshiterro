4f3db248dd7564fafe8adfbc61fd443d2d3fc3c5
yuki525 <entio777@gmail.com>
2021-07-13 08:13:28 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5c337398602986608785e95c76f4465e07b749d7b9f1414152b34f585f9e9bb40edee771f5afc873afad96391aefe704fc57148abfbcb355845c93506103bb51'
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   
   root to: 'homes#top'
 
+   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+   
 end
