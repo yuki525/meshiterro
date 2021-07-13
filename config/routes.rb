e99654a0c8d8fdf0250c220999b46aa0d08d871c
yuki525 <entio777@gmail.com>
2021-07-13 11:19:07 +0000

    [Finish] 4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'homes#top'
 
-   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-   
+  devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
 end
