db7c6e7b2008bcd00a2d6d379a1d08bace4b8a23
yuki525 <entio777@gmail.com>
2021-07-14 05:24:26 +0000

    [Finish] 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-
+  
+    resources :users, only: [:show, :edit, :update]
 end
